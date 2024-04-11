c25d5b08495fa0573ea8e13362fc177520117fb3
harish <harish@osmosys.co>
2024-04-11 17:30:33 +0530

    feat: update hardhat network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,4 +6,10 @@ require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.24',
+  networks: {
+    fantomtest: {
+      url: process.env.PROVIDER_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
